fb7a6481b2568fa8efdaef948c0a3a713f43ba16
root <root@eramba.org>
2023-01-26 17:31:30 +0100

    updated the code it now pulls users and groups from AWS

--- aws_account_reviews.php
+++ aws_account_reviews.php
@@ -2,18 +2,52 @@
 
 require 'vendor/autoload.php';
 
-use Aws\S3\S3Client;
+use Aws\Iam\IamClient;
 use GuzzleHttp\Client;
 
-/* first we pull accounts and their roles from AWS */
+
+$region = "eu-west-1";
+$profile = "aws_log_parser";
+$tmp_csv_file_name = "tmp.csv";
+$fp = fopen($tmp_csv_file_name, 'w');
+
+/* first we pull accounts and their roles from AWS 
+ * into an array that we can then easily push into a
+ * csv file */
 
 
-//Create an S3Client
-$iam_client = new Aws\S3\S3Client([
-    'version' => 'latest',
-    'region' => 'us-east-2'
+
+$aws = IamClient::factory(array(
+	'region' => $region,
+	'version' => 'latest',
+	'profile' => $profile,
+));
+
+$result = $aws->listUsers([
 ]);
 
+foreach($result['Users'] as $users) {
+
+	$username = $users['UserName'];
+
+	$tmp_array_groups = array();
+	$result_groups = $aws->listGroupsForUser([
+		'UserName' => $users['UserName'],
+	]);
+
+	foreach($result_groups['Groups'] as $groups) {
+		$group_name = $groups['GroupName'];
+		array_push($tmp_array_groups,$group_name);
+	}
+	$tmp_string_groups = implode("|",$tmp_array_groups);
+	$tmp_csv_row = array($username,$tmp_string_groups,"na");
+	fputcsv($fp,$tmp_csv_row);
+
+}
 
 
+
+
+fclose($fp);
+unlink($tmp_csv_file_name);
 ?>
