273c394a4af042423e1d58af4c923c020b3fd28a
root <root@eramba.org>
2023-01-26 18:09:34 +0100

    almost working

--- aws_account_reviews.php
+++ aws_account_reviews.php
@@ -1,21 +1,20 @@
 <?php
 
 require 'vendor/autoload.php';
+require 'credentials.php';
 
 use Aws\Iam\IamClient;
 use GuzzleHttp\Client;
 
 
-$region = "eu-west-1";
-$profile = "aws_log_parser";
-$tmp_csv_file_name = "tmp.csv";
-$fp = fopen($tmp_csv_file_name, 'w');
-
 /* first we pull accounts and their roles from AWS 
  * into an array that we can then easily push into a
  * csv file */
 
-
+$region = "eu-west-1";
+$profile = "aws_log_parser";
+$tmp_csv_file_name = "tmp.csv";
+$fp = fopen($tmp_csv_file_name, 'w');
 
 $aws = IamClient::factory(array(
 	'region' => $region,
@@ -40,14 +39,51 @@ foreach($result['Users'] as $users) {
 		array_push($tmp_array_groups,$group_name);
 	}
 	$tmp_string_groups = implode("|",$tmp_array_groups);
+	if ( empty($tmp_string_groups) ) { 
+		$tmp_string_groups = "na";
+	}
 	$tmp_csv_row = array($username,$tmp_string_groups,"na");
 	fputcsv($fp,$tmp_csv_row);
 
 }
 
-
-
-
 fclose($fp);
+
+
+/* now we connect to eramba and push the CSV file
+ * into the account review module 
+ * you need to create a file called
+ * credentials.php and inside copy paste the following
+ *
+ * $eramba_hostname = "";
+ * $eramba_username = "";
+ * $eramba_password = "";
+ *
+ * */
+
+$feed_id = "1";
+$feed_title = "AWS eramba account";
+$feed_description = "accounts used in AWS eramba";
+$feed_type = "1";
+
+$client = new GuzzleHttp\Client();
+
+$response = $client->request('POST', "$eramba_hostname/api/account-reviews/account-review-feeds/$feed_id", [
+
+	'auth' => [$eramba_username, $eramba_password],
+	'multipart' => [
+		[
+			'name'     => $tmp_csv_file_name,
+			'contents' => file_get_contents($tmp_csv_file_name),
+		]
+	]
+]);
+
+$code = $response->getStatusCode(); // 200
+$reason = $response->getReasonPhrase(); // OK
+
+echo "$code / $reason\n";
+
 unlink($tmp_csv_file_name);
+
 ?>
